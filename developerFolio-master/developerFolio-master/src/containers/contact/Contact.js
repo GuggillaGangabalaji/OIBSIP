a9ca379408cd3cbe24fa72ee23e240fdd0c65326
GuggillaGangabalaji <gangabalaji915@gmail.com>
2023-05-04 00:17:59 +0530

    balu

---
+++ developerFolio-master/developerFolio-master/src/containers/contact/Contact.js
@@ -0,0 +1,69 @@
+import React, {useContext} from "react";
+import "./Contact.scss";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import {illustration, contactInfo} from "../../portfolio";
+import {Fade} from "react-reveal";
+import email from "../../assets/lottie/email";
+import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Contact() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="20px">
+      <div className="main contact-margin-top" id="contact">
+        <div className="contact-div-main">
+          <div className="contact-header">
+            <h1 className="heading contact-title">{contactInfo.title}</h1>
+            <p
+              className={
+                isDark
+                  ? "dark-mode contact-subtitle"
+                  : "subTitle contact-subtitle"
+              }
+            >
+              {contactInfo.subtitle}
+            </p>
+            <div
+              className={
+                isDark ? "dark-mode contact-text-div" : "contact-text-div"
+              }
+            >
+              {contactInfo.number && (
+                <>
+                  <a
+                    className="contact-detail"
+                    href={"6301659583"}
+                  >
+                    {"6301659583"}
+                  </a>
+                  <br />
+                  <br />
+                </>
+              )}
+              <a
+                className="contact-detail-email"
+                href={"ganagabalaji915@gmail.com"}
+              >
+                {"ganagabalaji915@gmail.com"}
+              </a>
+              <br />
+              <br />
+              <SocialMedia />
+            </div>
+          </div>
+          <div className="contact-image-div">
+            {illustration.animated ? (
+              <DisplayLottie animationData={email} />
+            ) : (
+              <img
+                alt="Man working"
+                src={require("../../assets/images/contactMailDark.svg")}
+              ></img>
+            )}
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
